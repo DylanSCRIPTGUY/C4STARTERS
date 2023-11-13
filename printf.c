ca6590fc4c40d7774209fbe7d36e71cdbd24c9e3
DylanSCRIPTGUY <109195215+DylanSCRIPTGUY@users.noreply.github.com>
2023-11-12 17:14:26 -0800

    add logo and basics.html

---
+++ printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Essential for almost everything in C. Always add it
+
+
+int main() { // This is the start of the program.
+
+  printf("Hello world!\n"); // Printf is a function that prints text. \n creates a brand new line
+  
+  return 0; // this returns a value of 0. You must return a value before ending the program
+} // this is the end of the program.
